8dcdfec90028226c40fdf16a47503d0d72f3802c
PF94 on Vista <pf94@cloudsoftware.cf>
2019-12-28 14:30:19 -0500

    Revert "Empty "Brick icon" buttons and background images."
    This reverts commit 0fd9622d82ce8acfb985d47da71d51576ea8ba0b.

--- begingource.bat
+++ begingource.bat
@@ -1,1 +1,1 @@
-gource --title "BLOCKLAND: PROJECT-BETA" --highlight-users --user-scale 1.5 -e 0.01 --highlight-dirs --dir-font-size 12 -key -s 15
+gource --title "BLOCKLAND: PROJECT-BETA" --highlight-users --user-scale 1.5 -e 0.01 --highlight-dirs --dir-font-size 12 -key
